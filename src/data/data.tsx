2add356d1757d1410ebfde471358762f713a42af
JohnsJoking <johnyucel1@gmail.com>
2025-06-04 11:38:10 -0400

    Update data.tsx

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -73,14 +73,13 @@ export const heroData: Hero = {
   description: (
     <>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        I'm a Victoria based <strong className="text-stone-100">Full Stack Software Engineer</strong>, currently working
-        at <strong className="text-stone-100">Instant Domains</strong> helping build a modern, mobile-first, domain
-        registrar and site builder.
+        I'm a Northern Virginia based <strong className="text-stone-100">Business Systems Analyst</strong>, currently working
+        at <strong className="text-stone-100">CLVG Consulting with Accenture</strong> using this website to test my skills.
       </p>
       <p className="prose-sm text-stone-200 sm:prose-base lg:prose-lg">
-        In my free time time, you can catch me training in <strong className="text-stone-100">Muay Thai</strong>,
-        plucking my <strong className="text-stone-100">banjo</strong>, or exploring beautiful{' '}
-        <strong className="text-stone-100">Vancouver Island</strong>.
+        In my free time time, you can catch me making a <strong className="text-stone-100">coffee</strong>,
+        watching the latest <strong className="text-stone-100">shows and movies</strong>, or playing with my cat{' '}
+        <strong className="text-stone-100">Buddy AKA Boom Boom</strong>.
       </p>
     </>
   ),
@@ -108,12 +107,12 @@ export const aboutData: About = {
   to use or feel most comfortable with, describing your personality, or whatever else you feel like throwing
   in.`,
   aboutItems: [
-    {label: 'Location', text: 'Victoria, BC', Icon: MapIcon},
-    {label: 'Age', text: '29', Icon: CalendarIcon},
-    {label: 'Nationality', text: 'Canadian / Irish', Icon: FlagIcon},
-    {label: 'Interests', text: 'Motorcycles, Muay Thai, Banjos', Icon: SparklesIcon},
-    {label: 'Study', text: 'University of Victoria', Icon: AcademicCapIcon},
-    {label: 'Employment', text: 'Instant Domains, inc.', Icon: BuildingOffice2Icon},
+    {label: 'Location', text: 'McLean, VA', Icon: MapIcon},
+    {label: 'Age', text: '23', Icon: CalendarIcon},
+    {label: 'Nationality', text: 'Turkish / American', Icon: FlagIcon},
+    {label: 'Interests', text: 'Coffee, Cats, Movies & Shows', Icon: SparklesIcon},
+    {label: 'Study', text: 'Virginia Tech', Icon: AcademicCapIcon},
+    {label: 'Employment', text: 'CLVG Consulting', Icon: BuildingOffice2Icon},
   ],
 };
 
@@ -129,8 +128,8 @@ export const skills: SkillGroup[] = [
         level: 10,
       },
       {
-        name: 'French',
-        level: 4,
+        name: 'Turkish',
+        level: 7,
       },
       {
         name: 'Spanish',
@@ -139,10 +138,10 @@ export const skills: SkillGroup[] = [
     ],
   },
   {
-    name: 'Frontend development',
+    name: 'Network Engineering',
     skills: [
       {
-        name: 'React',
+        name: 'Linux',
         level: 9,
       },
       {
